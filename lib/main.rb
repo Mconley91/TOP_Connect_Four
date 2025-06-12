35119ea3173345fdfc3ec64dc78d3d092a47a453
mconley91 <matthew.e.conley@gmail.com>
2025-06-12 19:41:08 -0400

    add #make_board & #print_board along with tests for #make_board

--- lib/main.rb
+++ lib/main.rb
@@ -1,2 +1,25 @@
 # frozen_string_literal: true
 
+class Game 
+  attr_reader :game_board
+  
+  def initialize
+    @round = 1
+    @turn = 'Player 1'
+    @game_board
+  end
+
+  def make_board
+    @game_board = Array.new(6, Array.new(7, '_'))
+  end
+
+  def print_board
+    @game_board.each{|element| p element}
+  end
+
+end
+
+# test play area
+test_game = Game.new
+test_game.make_board
+test_game.print_board
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -1,2 +1,25 @@
 # frozen_string_literal: true
 
+require './lib/main.rb'
+
+describe Game do
+  subject(:new_game){described_class.new}
+  describe '#make_board' do
+    it 'create an array' do
+      new_game.make_board
+      expect(new_game.game_board).to be_a(Array)
+    end
+    it 'has 6 elements' do
+      new_game.make_board
+      expect(new_game.game_board.length).to be 6
+    end
+    it 'each element is a sub-array' do
+      new_game.make_board
+      expect(new_game.game_board.each{|array| array}).to be_a(Array)
+    end
+    it 'all sub-arrays have 7 elements' do
+      new_game.make_board
+      expect(new_game.game_board.all?{|array| array.length == 7}).to be true
+    end
+  end
+end
