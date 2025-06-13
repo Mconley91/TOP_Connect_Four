0625bd09bb10bfeba1fd7ef0124754f25d79e61e
mconley91 <matthew.e.conley@gmail.com>
2025-06-13 18:14:44 -0400

    working on #place_play

--- lib/main.rb
+++ lib/main.rb
@@ -1,23 +1,32 @@
 # frozen_string_literal: true
 
 class Game 
-  attr_reader :game_board
+  attr_accessor :game_board
   
   def initialize
     @round = 1
-    @player_turn = 'Player 1'
-    @game_board
+    @player_turn = 'X'
+    @game_board= []
   end
 
   def make_board
-    @game_board = Array.new(6, Array.new(7, '_'))
+    @game_board = Array.new(6){Array.new(7){"_"}}
   end
 
   def make_play
     loop do
       play = gets.chomp
       return play if play =~ /[1-7]/
-      p 'invalid input, try again!'
+      p 'invalid input!'
+    end
+  end
+
+  def place_play(column)
+    @game_board.reverse.each do|row| 
+      if row[column - 1] == '_'
+        row[column - 1] = @player_turn
+        return
+      end
     end
   end
 
@@ -42,8 +51,9 @@ class Game
   end
 end
 
-# test play area
+# test play area-------------
 # test_game = Game.new
 # test_game.make_board
-# test_game.display_game
 # test_game.make_play
+# test_game.place_play(3)
+# test_game.display_game
--- spec/main_spec.rb
+++ spec/main_spec.rb
@@ -3,29 +3,53 @@
 require './lib/main.rb'
 
 describe Game do
-  subject(:new_game){described_class.new}
   describe '#make_board' do
-  context 'makes a blank game board' do
-    before do
-      new_game.make_board
+    subject(:new_game){described_class.new}
+    context 'makes a blank game board' do
+      before do
+        new_game.make_board
+      end
+      it 'create an array' do
+        expect(new_game.game_board).to be_a(Array)
+      end
+      it 'has 6 elements' do
+        expect(new_game.game_board.length).to eq(6)
+      end
+      it 'each element is a sub-array' do
+        expect(new_game.game_board.each{|array| array}).to be_a(Array)
+      end
+      it 'all sub-arrays have 7 elements' do
+        expect(new_game.game_board.all?{|array| array.length == 7}).to be true
+      end
     end
-    it 'create an array' do
-      expect(new_game.game_board).to be_a(Array)
-    end
-    it 'has 6 elements' do
-      expect(new_game.game_board.length).to be 6
-    end
-    it 'each element is a sub-array' do
-      expect(new_game.game_board.each{|array| array}).to be_a(Array)
-    end
-    it 'all sub-arrays have 7 elements' do
-      expect(new_game.game_board.all?{|array| array.length == 7}).to be true
-    end
-  end
-    
   end
 
   describe '#make_play' do
-    
+    subject(:play_game){described_class.new}
+    context 'takes player input and returns it if valid'do
+      before do
+        allow(play_game).to receive(:gets).and_return('0','1')
+        allow(play_game).to receive(:p) #prevents p statements from printing to console
+      end
+      it 'prints a message to the console if input is invalid' do
+        expect(play_game).to receive(:p).with('invalid input!').once
+        play_game.make_play
+      end
+      it 'returns a valid input' do
+        expect(play_game.make_play).to eq('1')
+      end
+    end
   end
+
+  describe '#place_play' do
+    subject(:place_game){described_class.new}
+    context 'places a token in a given column' do
+      it 'places a token in the lowest row possible for that column' do
+        # column = 5
+        # place_game.place_play(column)
+        # expect(place_game.game_board[6][4]).to eq('X')
+      end
+    end
+  end
+
 end
